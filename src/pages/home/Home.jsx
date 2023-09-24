72d0048f2809bdddc153966dcfc0caf59bf70549
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-09-24 14:36:13 +0600

    Complete Home Page, used React.JS, Tailwind and many more

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Navbar from '../../components/navbar/Navbar'
+import Topbar from '../../components/topbar/Topbar'
+import Banner from '../../components/banner/Banner'
+import Shop from '../../components/shop/Shop'
+import Products from '../../components/products/Products'
+import Countdown from '../../components/countdown/Countdown'
+import FeaturedProducts from '../../components/featuredProducts/FeaturedProducts'
+import YTVideo from '../../components/ytVideo/YTVideo'
+import Testimonials from '../../components/testimonials/Testimonials'
+import Blog from '../../components/blog/Blog'
+import Footer from '../../components/footer/Footer'
+
+const Home = () => {
+    return (
+        <div>
+            <Topbar />
+            <Navbar />
+            <Banner />
+            <Shop />
+            <Products />
+            <Countdown />
+            <FeaturedProducts />
+            <YTVideo />
+            <Testimonials />
+            <Blog />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
